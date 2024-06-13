dd531ba765f2d17eced50ce1add5d6bb390e366d
abhiji4995 <abhijitshingade95@gmail.com>
2024-06-13 17:52:05 +0530

    css

---
+++ script.js
@@ -0,0 +1,85 @@
+// console.log("Welcome to javascript"); 
+
+// let message="the password is incorrect";
+// console.log(message);
+// alert(message);
+
+// let hello ="abhijit";
+// console.log(hello);
+
+// var newdata ="abc";
+// console.log(newdata);
+
+// let messageb;
+// messageb="world";
+// messageb="hello";
+// console.log(messageb);
+
+// let admin;
+// let name;
+// name ="abhijit";
+// admin = name;
+// alert(admin);
+
+// const birthday="08.12.2001";
+// birthday="10.11.2001";
+// console.log(birthday);
+
+// Data types in Javascript
+// 1.Number
+// let n=123;
+// let nf=12.44;
+// console.log(nf);
+
+// 2.Bigint
+// let bigint=1234567890n;
+// console.log(bigint);
+
+// 3.String
+// let str="hello";
+// let str2=`Single quotes are ok too`;
+// let phrase=`${str} can embed another ${str2}`;
+// console.log(phrase);
+// alert(`the result is ${1+2}`);
+
+// Boolean
+// let isGreater=4<6;
+// alert(isGreater);
+
+// 5.null
+// let age=null;
+// console.log(age);
+
+// 6.undefine
+// let data;
+// console.log(data);
+
+// Basic Operators
+// Addition
+// let apples="2";
+// let oranges="3";
+// alert(apples+oranges);
+
+// 1.Arithmatic Operator
+// a1=20;
+// a2=30;
+// a3=a1+a2;
+// console.log(a3);
+
+// 2.Assignment Operator
+// a1=20;
+// a1+=30;
+// console.log(a1);
+
+// let n=2;
+// n+=5;
+// n*=2;
+// alert(n);
+
+// let z=1, y=3;
+// alert(y-z);
+
+// let z=8, y=3;
+// alert(z/y);
+// alert(8%3);
+
